9f03e2e02c8683506917a629b72ba173d00809b3
Nicolas Rodriguez <nico@nicoladmin.fr>
2026-02-03 02:31:57 +0100

    feat: add with_env helper

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,3 +2,25 @@ require "spectator"
 require "crystal-env/spec"
 
 require "../src/envtpl"
+
+# see: https://github.com/crystal-lang/crystal/blob/master/spec/support/env.cr
+def with_env(values : Hash, &)
+  old_values = {} of String => String?
+  begin
+    values.each do |key, value|
+      key = key.to_s
+      old_values[key] = ENV[key]?
+      ENV[key] = value
+    end
+
+    yield
+  ensure
+    old_values.each do |key, old_value|
+      ENV[key] = old_value
+    end
+  end
+end
+
+def with_env(**values, &)
+  with_env(values.to_h) { yield }
+end
